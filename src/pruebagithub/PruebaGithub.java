18f2eb39c9ce58d7c87939ffff25fd74577e1251
Fernando Ciciliani <fernandociciliani@gmail.com>
2021-08-25 19:32:59 -0300

    Primer commit

---
+++ src/pruebagithub/PruebaGithub.java
@@ -0,0 +1,15 @@
+
+package pruebagithub;
+
+
+public class PruebaGithub {
+
+    
+    public static void main(String[] args) {
+
+       // Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+
+    }
+
+}
